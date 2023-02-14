f0259424a6a4d7b888663e8a0ecd5186e2ac1a14
Wataru Miyakoshi <wtrmiya@gmail.com>
2023-02-15 04:00:22 +0900

    retry: add .assign test

--- CombineRxSwiftPracticeTests/Assign_Bind_Tests.swift
+++ CombineRxSwiftPracticeTests/Assign_Bind_Tests.swift
@@ -6,30 +6,32 @@
 //
 
 import XCTest
+import Combine
 
 final class Assign_Bind_Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    // MARK: - Combine
+    func testAssign() {
+        
+        class SomeObject {
+            static let expectedValue = "Hello"
+            let expectation = XCTestExpectation(description: #function)
+            
+            var value: String = "" {
+                didSet {
+                    XCTAssertEqual(value, Self.expectedValue)
+                    expectation.fulfill()
+                }
+            }
         }
+        
+        let object = SomeObject()
+        let examplePublisher = Just(SomeObject.expectedValue)
+        
+        let cancellable = examplePublisher
+            .assign(to: \.value, on: object)
+        
+        wait(for: [object.expectation], timeout: 5)
+        XCTAssertNotNil(cancellable)
     }
-
 }
