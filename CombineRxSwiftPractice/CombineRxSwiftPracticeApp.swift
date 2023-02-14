3e07f7bb12fa2edbb8bb37d6115f3a012d1d513d
Wataru Miyakoshi <wtrmiya@gmail.com>
2023-02-14 16:46:26 +0900

    Initial Commit

---
+++ CombineRxSwiftPractice/CombineRxSwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineRxSwiftPracticeApp.swift
+//  CombineRxSwiftPractice
+//
+//  Created by Wataru Miyakoshi on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct CombineRxSwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
